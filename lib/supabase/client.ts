48b9f0755a64b6dba701d94966251fe20d6223a4
Eduardo Gurgel <eduardo@gurgel.me>
2026-06-06 04:25:26 +1200

    fix: 100% on 2.0.0 (#66)

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,12 +1,9 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  // Randomly pick between '1.0.0' and '2.0.0'
-  const vsn = Math.random() < 0.5 ? '1.0.0' : '2.0.0';
-
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY!,
-    { realtime: { vsn: vsn, heartbeatIntervalMs: 2000 } }
+    { realtime: { heartbeatIntervalMs: 2000 } }
   );
 }
